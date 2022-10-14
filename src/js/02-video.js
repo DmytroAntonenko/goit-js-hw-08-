57ff10c10288e252807f28d802a796bb27c7d393
DmytroAntonenko <d.antonenko81@gmail.com>
2022-10-14 10:12:11 +0300

    # rePlay

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,19 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const USEDTIME = "videoplayer-current-time";
+
 player.on('timeupdate', throttle(data, 1000));
 
+rePlay();
 
 function data(event) {
     localStorage.setItem(USEDTIME, event.seconds)
 }
+function rePlay() {
 const currentTime = localStorage.getItem(USEDTIME);
-player.setCurrentTime(currentTime);
+    if(currentTime) {
+    player.setCurrentTime(currentTime)
+    }
+}
+
+
